d54bb334878417e05e8b505531e470560da16171
Connie Leung <cleung334@gmail.com>
2023-11-26 22:35:11 +0800

    chore: read and write memory

--- assembly/index.ts
+++ assembly/index.ts
@@ -11,4 +11,27 @@ export function minusOne(n: i32): i32 {
   return n - 1;
 }
 
+export function fizzbuzz(n: i32): String | null {
+  log(n);
+
+  if (n % 15 === 0) {
+    return 'fizzbuzz';
+  } else if (n % 3 === 0) {
+    return 'fizz';
+  } else if (n % 5 === 0) {
+    return 'buzz';
+  }
+
+  return null;
+}
+
+memory.grow(2);
+store<u8>(0, 21);
+store<u8>(1, 99);
+
+export function readMemory(n: i32): i32 {
+  const valueAtIndexOne = load<u8>(n);
+  return valueAtIndexOne;
+}
+
 export * from './math';
--- js/loader.js
+++ js/loader.js
@@ -18,11 +18,12 @@ class WasmLoader {
 
         console.log('Fetching', path, 'imports', imports);
 
-        if (!WebAssembly.instantiateStreaming) {
+        if (!loader.instantiateStreaming) {
             return this.wasmFallback(path, imports);
         }
 
-        const { instance } = await WebAssembly.instantiateStreaming(fetch(path), imports);
+        const instance = await loader.instantiateStreaming(fetch(path), imports);
+        console.log('instance', instance);
 
         return instance?.exports;
     }
@@ -32,7 +33,7 @@ class WasmLoader {
 
         const response = await fetch(path);
         const bytes = await response?.arrayBuffer();
-        const { instance } = await WebAssembly.instantiate(bytes, imports);
+        const instance = await loader.instantiate(bytes, imports);
 
         return instance?.exports;
     }
